633c6db76676b7a87ebd00019e0c1e17f5a2f4dc
Alexander Arnold <alexander@galvezgil.com>
2024-10-23 18:40:33 +0200

    safari bug, make onmounted inside headersection

--- resources/js/Components/Layouts/Layout1/HeaderSection.vue
+++ resources/js/Components/Layouts/Layout1/HeaderSection.vue
@@ -2,10 +2,39 @@
 import LanguageSwitcher from '@/Components/LanguageSwitcher.vue';
 import { useI18n } from 'vue-i18n';
 import { getCountryName } from '@/utils/localizedSlugMixin';
+import { onMounted } from 'vue';
 
 const { locale } = useI18n();
-
 const country = getCountryName();
+
+onMounted(() => {
+        const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+
+        function mobileNavToogle() {
+            document.querySelector('body').classList.toggle('mobile-nav-active');
+            mobileNavToggleBtn.classList.toggle('bi-list');
+            mobileNavToggleBtn.classList.toggle('bi-x');
+        }
+        mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+
+        let scrollTop = document.querySelector('.scroll-top');
+
+        function toggleScrollTop() {
+            if (scrollTop) {
+            window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+            }
+        }
+        scrollTop.addEventListener('click', (e) => {
+            e.preventDefault();
+            window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+            });
+        });
+
+        window.addEventListener('load', toggleScrollTop);
+        document.addEventListener('scroll', toggleScrollTop);
+});
 </script>
 
 <template>
