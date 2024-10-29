50ae8502878b706ce359aab5a30103498b1f8e8d
Alexander Arnold <alexander@galvezgil.com>
2024-10-29 13:49:59 +0100

    namensraum google search console ändern

--- app/Console/Commands/GenerateSitemapCommand.php
+++ app/Console/Commands/GenerateSitemapCommand.php
@@ -125,7 +125,7 @@ class GenerateSitemapCommand extends Command
     private function generateMainSitemap($sitemaps, $domain)
     {
         $mainContent = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-        $mainContent .= "<sitemapindex xmlns=\"http://www.sitemaps.org/schemas/sitemap-image/1.0\">\n";
+        $mainContent .= "<sitemapindex xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
 
         foreach ($sitemaps as $locale) {
             $mainContent .= "<sitemap>\n";
