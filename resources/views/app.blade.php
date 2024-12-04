5048a589828edbb8ab01a3d4b7553bca1d19d743
Alexander Arnold <alexander@galvezgil.com>
2024-12-04 15:06:27 +0100

    chanes favicons for Bing, update posthog-js

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,15 +18,20 @@
         <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
-        <!-- Vendor CSS Files -->
+        <!-- CSS Files -->
         <link href="{{ asset('vendor/aos/aos.css') }}" rel="stylesheet">
         <link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
         <link href="{{ asset('vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
         <link href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('img/favicon.ico') }}" rel="icon">
-        <link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+        <link rel="icon" type="image/png" href="/img/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/img/favicon.svg" />
+        <link rel="shortcut icon" href="/img/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
+        <link rel="manifest" href="/img/site.webmanifest" />
 
         <!-- Scripts -->
+        <script defer src="https://app.fastbots.ai/embed.js" data-bot-id="cm45i90e50iehsvbm925x79ow"></script>
+
         @routes
         @vite([
             "resources/js/app.js",
